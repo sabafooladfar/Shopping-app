d366db535762e5802ec4dbcdd7e045e9e21dea92
saba fooladfar <fooladfarsaba@yahoo.com>
2023-03-28 11:35:15 +0430

    render products in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,39 @@
+import * as data from "../data";
 import Layout from "../Layout/Layout";
 
 const HomePage = () => {
+
+const addToCartHandler =(product)=>{
+  console.log(product)
+}
+
   return (
     <Layout>
-      <div>this is home page</div>
+      <main className="container">
+        <section className="productList">
+          {data.products.map((product) => {
+            return (
+              <div className="product">
+                <div className="productImg">
+                <img src={product.image} />
+                </div>
+                <div className="productDesc">
+                  <p>{product.name}</p>
+                  <div>
+                    <p>Price: {product.price} $</p>
+                    {product.discount !== 0 ? (
+                      <p>Sale : {product.offPrice} $</p>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <button onClick={()=>addToCartHandler(product)} className="addToCartBtn">Add To Cart</button>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
   );
 };
